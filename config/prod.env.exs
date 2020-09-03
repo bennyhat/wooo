fee942dc3d43f770bf019e0dee3ccbf9b25ff50a
Benjamin Brewer <ben@benbrewer.me>
2020-09-03 14:53:22 -0400

    fix origins

--- config/prod.env.exs
+++ config/prod.env.exs
@@ -28,11 +28,21 @@ port =
   Please set it to what you expect the production site to be accessed with
   """
 
+allowed_origins =
+  System.get_env("ALLOWED_ORIGINS") ||
+  raise """
+  environment variable ALLOWED_ORIGINS is missing.
+  Please set it to what you expect the production site to be accessed with
+  """
+
+allowed_origins_list = String.split(allowed_origins, ",")
+
 config :wooo, WoooWeb.Endpoint,
   http: [
     port: String.to_integer(port),
     transport_options: [socket_opts: [:inet6]]
   ],
+  check_origin: allowed_origins_list,
   url: [host: hostname, port: 443],
   secret_key_base: secret_key_base,
   live_view: [signing_salt: live_view_signing_salt]
